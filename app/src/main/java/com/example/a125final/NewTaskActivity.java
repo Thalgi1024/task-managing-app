392cc073aa79cef837fb465826ebf11c5485910b
ORaNgCHiKeN <justink6@illinois.edu>
2019-12-02 21:42:49 -0600

    New task creation UI

--- app/src/main/java/com/example/a125final/NewTaskActivity.java
+++ app/src/main/java/com/example/a125final/NewTaskActivity.java
@@ -3,6 +3,7 @@ package com.example.a125final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class NewTaskActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class NewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        Button createTaskButton = findViewById(R.id.createTaskButton);
+        createTaskButton.setOnClickListener(unused -> {
+            System.out.println("Created Task");
+        });
     }
 }
