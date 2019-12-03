d871e260ce9562d5da6c35a2cc6c33ae11ca5a2a
cyx2 <cyx2@illinois.edu>
2019-12-03 02:43:36 -0600

    Debugged, added button functionality

--- app/src/main/java/com/example/a125final/MainActivity.java
+++ app/src/main/java/com/example/a125final/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.a125final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton addTaskButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Your Tasks");
+        addTaskButton = findViewById(R.id.createTaskButton);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                System.out.println("Hello There");
+                addTask();
+            }
+
+        });
+    }
+
+    private void addTask() {
     }
 }
